c03f57456849584c8798c6a03da66b0c4c1b236d
rajibdas-it <rajibrad@gmail.com>
2022-11-05 17:09:16 +0600

    order collections done

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 require("colors");
-const data = require("./Data/data.json");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -25,9 +24,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const productCollection = client.db("myShop").collection("product");
+    const orderCollection = client.db("myShop").collection("orders");
+
     app.get("/products", async (req, res) => {
       const query = {};
       const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+    app.get("/orders", async (req, res) => {
+      const query = { email: req.query.email };
+      const cursor = orderCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -35,10 +55,6 @@ async function run() {
   }
 }
 run().catch((error) => console.log(error));
-
-app.get("/products", (req, res) => {
-  res.send(data);
-});
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`.cyan);
